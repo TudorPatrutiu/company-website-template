79bf8488abc5a5569a49062e88ff6eab1f40d3f4
Tudor Patrutiu <patrutiu.tudor@gmail.com>
2023-12-05 19:45:10 +0200

    Changed the dirs structure

--- act-before-react/src/components/Services.js
+++ act-before-react/src/components/Services.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { FaReact } from 'react-icons/fa'; // Example for React icon
 import './Services.scss'; // Assuming you have an SCSS file for styles
 
-<FaReact size={30} />
+
 
 function Services() {
   return (
     <div className="services">
-      {/* Content for Services will go here */}
+      <div className="tech-icon"><FaReact /> React</div>
+      <div className="tech-icon"><FaNodeJs /> Node.js</div>
+      {/* Add more technologies */}
     </div>
   );
 }
